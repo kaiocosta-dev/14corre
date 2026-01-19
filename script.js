469ea4c7216a3a9945a4d3d963834e1a8c021ee3
Kaio <kaiocostadossantos@gmail.com>
2026-01-19 14:34:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -33,8 +33,67 @@ function toggleText(id, btn) {
     btn.innerText = expanded ? "Ler menos" : "Ler mais...";
 }
 
+function initParticles() {
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#4B0082"
+            },
+            "shape": {
+                "type": "circle"
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false
+            },
+            "size": {
+                "value": 3,
+                "random": true
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ccc",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            }
+        },
+        "retina_detect": true
+    });
+}
+
 window.onload = () => {
     loadLogo();
     typeText('typewriter');
-
+    initParticles();
 };
